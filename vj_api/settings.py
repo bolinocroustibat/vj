1c226b345a786fb80fc26dd472342dc0893b02d3
Adrien Carpentier <me@adriencarpentier.com>
2023-02-16 17:14:14 +0900

    Add Sentry experimental profiling

--- vj_api/settings.py
+++ vj_api/settings.py
@@ -156,5 +156,11 @@ sentry_sdk.init(
 
 	# If you wish to associate users to errors (assuming you are using
 	# django.contrib.auth) you may enable sending PII data.
-	send_default_pii=True
+	send_default_pii=True,
+
+	# Experimental profiling
+	_experiments={
+		"profiles_sample_rate": 1.0,
+	}
+
 )
