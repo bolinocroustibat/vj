8a690c8c9da367d96c0f802a69f406d3260c9345
Adrien Carpentier <me@adriencarpentier.com>
2023-06-29 15:04:30 +0200

    feat: use postgresql

--- vj_api/helpers/json_renderer.py
+++ vj_api/helpers/json_renderer.py
@@ -6,6 +6,7 @@ class ORJSONRenderer(BaseRenderer):
     """
     JSON Renderer using orjson (around 6 times faster than the default json module)
     """
+
     media_type = "application/json"
 
     def render(self, request, data: dict, *, response_status) -> str:
