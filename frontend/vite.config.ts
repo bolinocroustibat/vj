5a914ec8a2748c910c7ea97b20dfbd1a55cdff54
Adrien Carpentier <me@adriencarpentier.com>
2025-06-21 10:58:32 +0900

    fix: fix vite config to allow production host

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
 	preview: {
 		port: 4173,
 		host: "0.0.0.0",
+		allowedHosts: ["vj.adriencarpentier.com", "localhost", "127.0.0.1"],
 	},
 	build: {
 		assetsDir: "assets",
